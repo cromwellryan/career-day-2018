45abd9e665cf622baa48d51d1b7bc280716553ca
Ryan Cromwell <ryan@heysparkbox.com>
2018-11-18 19:08:02 -0500

    Receive SMS messages for fun and profit

--- server/index.js
+++ server/index.js
@@ -1,7 +1,35 @@
 const express = require('express')
 const app = express()
-const port = process.env.PORT || 3000
+const bodyParser = require('body-parser');
 
+const MessagingResponse = require('twilio').twiml.MessagingResponse;
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+/*********************************
+ * This is our website. 
+ * Do you like it?
+ * ******************************/
 app.use(express.static('public'))
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+/*********************************
+ * Receive messages from twilio!
+ * ******************************/
+app.post('/sms', (req, res) => {
+  const twiml = new MessagingResponse();
+
+  console.log('SMS received!!!');
+
+  if (req.body.Body.toLowerCase().indexOf('robot') > -1) {
+    twiml.message('Robots are awesome! 🤖🤖🤖');
+
+    res.writeHead(200, {'Content-Type': 'text/xml'});
+    res.end(twiml.toString());
+  } else {
+    res.writeHead(200, {'Content-Type': 'text/xml'});
+    res.end()
+  }
+});
+
+const port = process.env.PORT || 3000
+app.listen(port, () => console.log(`Listening on port ${port}!`))
